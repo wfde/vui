772c29bb28a030b3ad0c33e481ea9a9bcf32816d
huangwengen <262281610@qq.com>
2019-06-02 00:20:19 +0800

    ts检测

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,8 +35,6 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
-  public created() {
-  }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,85 @@
 module.exports = {
+
+    // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+    // baseUrl: process.env.NODE_ENV === "production" ? "./" : "/",
+
+    // 把开发服务器架设在根路径
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+
+    // 生成文件的目录名称
+    outputDir: "dist",
+
+    //用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+    assetsDir: "static",
+
+    // 默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。
+    filenameHashing: true,
+
+    // 不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建,map就是为了方便打印错误位置。
+    productionSourceMap: false,
+
+    // 设置单页面与多页面
+    // 多页面情况下，每个“page”应该有一个对应的 JavaScript 入口文件。其值应该是一个对象，对象的 key 是入口的名字，value 是：
+    // 一个指定了 entry, template, filename, title 和 chunks 的对象 (除了 entry 之外都是可选的)；
+    // 或一个指定其 entry 的字符串。
+    // 具体情况看官网 https://cli.vuejs.org/zh/config/#pages
     pages: {
         index: {
             // page 的入口
-            entry: 'src/main.ts'
-        },
-        // 当使用只有入口的字符串格式时，
-        // 模板会被推导为 `public/subpage.html`
-        // 并且如果找不到的话，就回退到 `public/index.html`。
-        // 输出文件名会被推导为 `subpage.html`。
+            entry: 'src/main.ts',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            title: 'Vui库',
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        }
     },
-    lintOnSave: 'error'
-}
+
+    //默认情况下，只有以文件结尾的文件*.module.[ext]才会被视为CSS模块。将此设置为true允许您.module放入文件名并将所有*.(css|scss|sass|less|styl(us)?)文件视为CSS模块。
+    //extract true在生产中，false在开发中,是否将组件中的CSS提取到独立的CSS文件中（而不是在JavaScript中内联并动态注入,在开发模式下禁用提取CSS，因为它与CSS热重新加载不兼容
+    //sourceMap是否为CSS启用源映射。将此设置为true可能会影响构建性能
+    //将选项传递给与CSS相关的加载器
+    css:{
+        modules:false,
+        extract:true,
+        sourceMap:false,
+        loaderOptions:{
+            css: {
+                // options here will be passed to css-loader
+            },
+            postcss: {
+                // options here will be passed to postcss-loader
+            }
+        }
+    },
+
+    // 是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。
+    // 具体信息看官网 https://cn.vuejs.org/v2/guide/installation.html#运行时+编译器vs.只包含运行时
+    runtimeCompiler: false,
+
+    // 通过设置让浏览器 overlay 同时显示警告和错误 在 devServe 配置项中  ==>  overlay: {warnings: true,errors: true}
+    lintOnSave: 'error',
+
+    devServer: {
+        // quiet: true,
+        overlay: {
+            warnings: true,
+            errors: true
+        }
+    },
+
+    chainWebpack: config => {
+
+        //在单独的进程中，运行打字类型检查器的web pack插件
+        config.plugin('fork-ts-checker').tap(([options]) => {
+            return [{
+                //以下几项都是推荐配置，async false使其可以在 页面上显示
+                async: false,
+                tslint: true,
+                vue: true
+            }]
+        })
+    }
+};
