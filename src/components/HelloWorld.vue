227a8f1e1d07e98cfda2fb55b8ae53f116ce7435
hwg <262281610@qq.com>
2019-06-07 16:51:15 +0800

    优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="hello">
         <h1>{{ msg }} </h1>
-        <p>{{ str }}</p>
     </div>
 </template>
 
@@ -10,17 +9,7 @@
 
     @Component
     export default class HelloWorld extends Vue {
-        str = '';
-        name = 'hwg';
         @Prop() private msg!: string;
-
-        created() {
-            this.sayHello(this.name);
-        }
-
-        sayHello(name: string) {
-            this.str = '您好,' + name;
-        }
     }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <!--<img alt="Vue logo" src="../assets/logo.png">-->
+        <HelloWorld msg="基于Vue.js开发的UI库 - Vui"/>
     </div>
 </template>
 
