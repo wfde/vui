a9c94b2b035cc3bf017afd3a2d107425a48a1905
huangwengen <262281610@qq.com>
2019-06-08 22:19:02 +0800

    目录结构调整

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from '@/App.vue';
 import router from '@/router';
-import store from './store';
-import Vui from './index';
+import store from '@/store';
+import Vui from '@/index';
 
 Vue.use(Vui);
 Vue.config.productionTip = false;
