5e7738034c8109300a77a7f8bd064cc6cb9d5c5b
Jordonwang <jordonwang@qq.com>
2019-06-22 18:56:05 +0800

    feat 新增input组件

---
+++ src/components/input/src/input.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="v-input">
+        <input class="v-input-inner" :type="type" :placeholder="placeholder">
+
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class VInput extends Vue {
+
+        @Prop({
+            type: String,
+            default: 'text'
+        })
+        type: string | undefined;
+
+        @Prop({
+            type: String,
+            default: '请输入内容'
+        })
+        placeholder: string | undefined;
+    }
+
+</script>
+
+<style scoped lang="scss">
+    .v-input{
+        display: inline-block;
+        width: 200px;
+        position: relative;
+        .v-input-inner{
+            background-color: #fff;
+            background-image: none;
+            border-radius: 4px;
+            border: 1px solid #dcdfe6;
+            box-sizing: border-box;
+            color: #606266;
+            display: inline-block;
+            font-size: inherit;
+            height: 40px;
+            line-height: 40px;
+            outline: none;
+            padding: 0 15px;
+            transition: border-color .2s cubic-bezier(.645,.045,.355,1);
+            width: 100%;
+            &:focus{
+                outline: none;
+                border-color: #409eff;
+            }
+        }
+    }
+</style>
---
+++ src/views/components/input.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="input-page">
+        <v-input></v-input>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+</script>
+
+<style scoped>
+
+</style>
