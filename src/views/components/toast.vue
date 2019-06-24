57e50361df3710b7381d52c4ef72c346d257edb8
hwg <262281610@qq.com>
2019-06-24 20:23:34 +0800

    Docs: 演示文档

--- src/views/components/toast.vue
+++ src/views/components/toast.vue
@@ -1,9 +1,24 @@
 <template>
     <div class="toast-page">
         <h1>{{this.$route.meta.title}}</h1>
-        <p><a href="javascript:;" @click="openToast('top','success','恭喜你，这是一条成功消息')">成功-top</a></p>
-        <p><a href="javascript:;" @click="openToast('middle','error','抱歉，这是一条错误消息')">失败-middle</a></p>
-        <p><a href="javascript:;" @click="openToast('middle','warning','提示，这是一条警告消息')">警告-bottom</a></p>
+        <div>
+            <h3>成功</h3>
+            <a href="javascript:;" @click="openToast('top','success','恭喜你，这是一条成功消息')">上</a>
+            <a href="javascript:;" @click="openToast('middle','success','恭喜你，这是一条成功消息')">中</a>
+            <a href="javascript:;" @click="openToast('bottom','success','恭喜你，这是一条成功消息')">下</a>
+        </div>
+        <div>
+            <h3>错误</h3>
+            <a href="javascript:;" @click="openToast('top','error','抱歉，这是一条错误消息')">上</a>
+            <a href="javascript:;" @click="openToast('middle','error','抱歉，这是一条错误消息')">中</a>
+            <a href="javascript:;" @click="openToast('bottom','error','抱歉，这是一条错误消息')">下</a>
+        </div>
+        <div>
+            <h3>警告</h3>
+            <a href="javascript:;" @click="openToast('top','warning','提示，这是一条警告消息')">上</a>
+            <a href="javascript:;" @click="openToast('middle','warning','提示，这是一条警告消息')">中</a>
+            <a href="javascript:;" @click="openToast('bottom','warning','提示，这是一条警告消息')">下</a>
+        </div>
     </div>
 </template>
 
