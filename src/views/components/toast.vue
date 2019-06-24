d50efec1936e627d3485a030dd0c2abe1f3e06c6
hwg <262281610@qq.com>
2019-06-24 16:15:56 +0800

    Feat: 新增类型提示框

--- src/views/components/toast.vue
+++ src/views/components/toast.vue
@@ -1,25 +1,26 @@
 <template>
-  <div class="toast-page">
-    <h1>{{this.$route.meta.title}}</h1>
-    <a href="javascript:;" @click="openToast('top','提交成功')">top</a>
-    <a href="javascript:;" @click="openToast('middle','提交成功')">middle</a>
-    <a href="javascript:;" @click="openToast('bottom','提交成功')">bottom</a>
-  </div>
+    <div class="toast-page">
+        <h1>{{this.$route.meta.title}}</h1>
+        <p><a href="javascript:;" @click="openToast('top','success','恭喜你，这是一条成功消息')">成功-top</a></p>
+        <p><a href="javascript:;" @click="openToast('middle','error','抱歉，这是一条错误消息')">失败-middle</a></p>
+        <p><a href="javascript:;" @click="openToast('bottom','warning','提示，这是一条警告消息')">警告-bottom</a></p>
+    </div>
 </template>
 
 <script lang="ts">
-  import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Vue} from 'vue-property-decorator';
 
-  @Component
-  export default class Toast extends Vue {
+    @Component
+    export default class Toast extends Vue {
 
-    openToast(name: string, msg: string){
-        this.$toast({
-            position: name,
-            message: msg
-        });
+        openToast(name: string, ty: string, msg: string) {
+            this.$toast({
+                message: msg,
+                position: name,
+                type: ty
+            });
+        }
+
     }
-
-  }
 
 </script>
