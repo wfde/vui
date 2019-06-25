2eed3f8b6ffdb4f5244a020da7408470a4b271fe
Jordonwang <jordonwang@qq.com>
2019-06-25 21:47:47 +0800

    refactor: input组件样式提取

--- src/vui/components/input/src/input.vue
+++ src/vui/components/input/src/input.vue
@@ -1,7 +1,7 @@
 <template>
     <div :class="{
         'v-input': true,
-        'clearable': clearable === true
+        'clearable': clearable === true && value
     }">
         <input class="v-input-inner"
                :type="type"
@@ -9,7 +9,7 @@
                :disabled="disabled"
                v-model="value"
                @input="inputHandle">
-        <span v-if="clearable" class="v-input-clearable" @click="clearInputValue"></span>
+        <span v-if="clearable && value" class="v-input-clearable" @click="clearInputValue"></span>
     </div>
 </template>
 
@@ -58,71 +58,3 @@
     }
 
 </script>
-
-<style scoped lang="scss">
-    .v-input{
-        display: inline-block;
-        width: 200px;
-        position: relative;
-        .v-input-inner{
-            background-color: #fff;
-            background-image: none;
-            border-radius: 4px;
-            border: 1px solid #dcdfe6;
-            box-sizing: border-box;
-            color: #606266;
-            display: inline-block;
-            font-size: inherit;
-            height: 40px;
-            line-height: 40px;
-            outline: none;
-            padding: 0 15px;
-            transition: border-color .2s cubic-bezier(.645,.045,.355,1);
-            width: 100%;
-            &:focus{
-                outline: none;
-                border-color: #409eff;
-            }
-            &:disabled{
-                background: #f5f5f5;
-                cursor: not-allowed;
-            }
-        }
-        .v-input-clearable{
-            position: absolute;
-            width: 14px;
-            height: 14px;
-            border-radius: 50%;
-            right: 10px;
-            top: 50%;
-            transform: translateY(-50%);
-            cursor: pointer;
-            &:hover{
-                &::after,&::before{
-                    background: #000;
-                }
-            }
-            &::after,&::before{
-                content: '';
-                display: block;
-                background: #ccc;
-                width: 1px;
-                height: 10px;
-                position: absolute;
-                top: 1px;
-                left: 8px;
-            }
-            &::after{
-                transform: rotate(45deg);
-            }
-            &::before{
-                transform: rotate(-45deg);
-            }
-        }
-    }
-    .clearable{
-        .v-input-inner{
-            padding-right: 24px;
-        }
-    }
-</style>
